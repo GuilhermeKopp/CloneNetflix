68ad24f829e877e9c57b24025d8caaea2a4ac7e8
GuilhermeKopp <guikopp19@gmail.com>
2021-11-15 16:36:15 -0300

    Capa principal

--- src/Tmdb.js
+++ src/Tmdb.js
@@ -2,7 +2,7 @@
 const API_KEY = '894299d830ff173f687394248c8c5d2b';
 const API_BASE = 'https://api.themoviedb.org/3';
 
-const basicFetch = async (endpoint) => {
+const basicFecth = async (endpoint) => {
     const req = await fetch (`${API_BASE}${endpoint}`);
     const json = await req.json();
     return json;
@@ -12,45 +12,64 @@ export default {
     getHomeList: async () => {
         return [
             {
-                slug: 'Originals',
+                slug: 'originals',
                 title: 'Originais da Netflix',
-                items: await basicFetch(`/discover/tv?with_networks=213&language=pt-BR&api_key=${API_KEY}`)
+                items: await basicFecth(`/discover/tv?with_networks=213&language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'trending',
                 title: 'Recomendados para você',
-                items: await basicFetch(`/trending/all/week?language=pt-BR&api_key=${API_KEY}`)
+                items: await basicFecth(`/trending/all/week?language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'toprated',
                 title: 'Em alta',
-                items: await basicFetch(`/movie/top_rated?language=pt-BR&api_key=${API_KEY}`)
+                items: await basicFecth(`/movie/top_rated?language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'action',
                 title: 'Ação',
-                items: await basicFetch(`/discover/movie?with_genres=28&language=pt-BR&api_key=${API_KEY}`)
+                items: await basicFecth(`/discover/movie?with_genres=28&language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'comedy',
                 title: 'Comédia',
-                items: await basicFetch(`/discover/movie?with_genres=35&language=pt-BR&api_key=${API_KEY}`)
+                items: await basicFecth(`/discover/movie?with_genres=35&language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'horror',
                 title: 'terror',
-                items: await basicFetch(`/discover/movie?with_genres=27&language=pt-BR&api_key=${API_KEY}`)
+                items: await basicFecth(`/discover/movie?with_genres=27&language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'romance',
                 title: 'Romance',
-                items: await basicFetch(`/discover/movie?with_genres=10749&language=pt-BR&api_key=${API_KEY}`)
+                items: await basicFecth(`/discover/movie?with_genres=10749&language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'documentary',
                 title: 'Documentários',
-                items: await basicFetch(`/discover/movie?with_genres=99&language=pt-BR&api_key=${API_KEY}`)
+                items: await basicFecth(`/discover/movie?with_genres=99&language=pt-BR&api_key=${API_KEY}`)
             }
         ];
+    },
+    getMovieInfo: async (movieId, type) => {
+        let info = {};
+
+        if(movieId) {
+            switch(type){
+                case 'movei':
+                    info = await basicFecth(`/movie/${movieId}?language=pt-BR&api_key=${API_KEY}`);
+                break;
+                 case 'tv':
+                    info = await basicFecth(`/tv/${movieId}?language=pt-BR&api_key=${API_KEY}`);
+                break;
+                default:
+                    info = null;
+                break;
+            }
+        }
+
+        return info;
     }
 }
---
+++ src/components/FeaturedMovie.js
@@ -0,0 +1,17 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from "react";
+import './FeaturedMovie.css';
+
+export default ({item}) => {
+    return (
+        <section className="featured" style={{
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            backgroundImage: `url(https://image.tmdb.org/t/p/original${item.backdrop_path})`
+        }}>
+            <div className="featured--vertical">
+                
+            </div>
+        </section>
+    );
+}
